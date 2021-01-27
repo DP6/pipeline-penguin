9f97e55f7853373b0c845b562404d417fdda14ad
Bruno Munhoz <bruno.munhoz@dp6.com.br>
2021-01-27 13:04:05 -0300

    V1 - Feature 1 - Adding Methods Parameters
    Parameters added on every method that needs for this feature.
    Added list_nodes method on NodeManager.
    duplicate_node became copy_node.
    Altered some docstrings.

--- pipeline-penguin/data_node_bigquery.py
+++ pipeline-penguin/data_node_bigquery.py
@@ -1,10 +1,11 @@
 class data_node_bigquery(DataNode):
-  def __init__(self, name, source, project_id, dataset_id, table_id):
+  def __init__(self, name, project_id, dataset_id, table_id, service_account_json):
     '''
       Constructor for the data_node_bigquery.
       Must receive BigQuery table data.
     '''
-    super().__init__(self, name, source)
+    super().__init__(self, name, "BigQuery")
     self.project_id = project_id
     self.dataset_id = dataset_id
     self.table_id = table_id
+    self.service_account_json = service_account_json
--- pipeline-penguin/pipeline_penguin.py
+++ pipeline-penguin/pipeline_penguin.py
@@ -1,6 +1,6 @@
 class pipeline_penguin:
   def __init__(self):
     '''
-      Constructor for the pipeline_penguin
+      Constructor for the pipeline_penguin.
     '''
     self.nodes = NodeManager()
