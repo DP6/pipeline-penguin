26176a2712c3f8df883ecbfcff4c7d7918f18e02
Gabrielly de Andrade <gabrielly.andrade@dp6.com.br>
2021-02-09 18:10:28 -0300

    16298 refactor: modify implementation of create_node method

--- pipeline_penguin/exceptions.py
+++ pipeline_penguin/exceptions.py
@@ -1,5 +1,14 @@
-class NodeManagerMissingArgs(TypeError):
+class NodeManagerMissingCorrectArgs(TypeError):
     """
     Raised when there are missing arguments in DataNode __init__
     """
+
+    pass
+
+
+class NodeTypeNotFound(KeyError):
+    """
+    Raised when node type was not found
+    """
+
     pass
