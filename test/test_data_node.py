9d527c763e0c6e7a8038ba3118034ea5edcc707b
Bruno Munhoz <bruno.munhoz@dp6.com.br>
2021-03-04 15:46:51 -0300

    Test/16300 Removing unused imports and tests

--- test/test_data_node.py
+++ test/test_data_node.py
@@ -1,8 +1,5 @@
-from pipeline_penguin import PipelinePenguin
-from pipeline_penguin.node_manager import NodeManager
 from pipeline_penguin.data_node import DataNode
 
 
 class TestDataNode:
-    def test_instance_type(self):
-        assert isinstance(DataNode("test", "test"), DataNode)
+    pass
--- test/test_data_node_bigquery.py
+++ test/test_data_node_bigquery.py
@@ -1,22 +1,8 @@
-from pipeline_penguin import PipelinePenguin, NodeType
-from pipeline_penguin.node_manager import NodeManager
 from pipeline_penguin.data_node import DataNode
 from pipeline_penguin.data_node_bigquery import DataNodeBigQuery
 
 
 class TestDataNodeBigQuery:
-    def test_instance_type(self):
-        assert isinstance(
-            DataNodeBigQuery(
-                "name_test",
-                "project_test",
-                "dataset_test",
-                "table_test",
-                "account_test",
-            ),
-            DataNodeBigQuery,
-        )
-
     def test_instance_superclass_type(self):
         assert isinstance(
             DataNodeBigQuery(
