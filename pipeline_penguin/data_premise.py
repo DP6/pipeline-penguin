3e345d45228e234f1dd0886c7291eda55a8e1cee
Bruno Munhoz <bruno.munhoz@dp6.com.br>
2021-03-04 15:41:45 -0300

    chore/16091-inserting-new-structures-for-f2
    chore/16298 [V1][F2][Documentação] Transcrição UML para documentação técnica - Feature 2

---
+++ pipeline_penguin/data_premise.py
@@ -0,0 +1,8 @@
+class DataPremise:
+    def __init__(self, name, type, column):
+        """
+        Constructor for the data_premise.
+        """
+        self.name = name
+        self.type = type
+        self.column = column
