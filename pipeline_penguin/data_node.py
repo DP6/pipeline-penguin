0cdecd8b8e358f76f4622fab916ad652ce96b12f
Gabrielly de Andrade <gabrielly.andrade@dp6.com.br>
2021-03-10 19:24:32 -0300

    18843 feat: adding insert premise

--- pipeline_penguin/data_node.py
+++ pipeline_penguin/data_node.py
@@ -1,23 +1,31 @@
+from typing import Callable, Dict, Type
+
+from pipeline_penguin.data_premise import DataPremise
+
+PremiseFactory = Callable[..., Type[DataPremise]]
+
+
 class DataNode:
-    def __init__(self, name, source):
+    def __init__(self, name: str, source: str):
         """
         Constructor for the DataNode.
         Must check if the source is a valid, supported data source.
         """
         self.name = name
         self.source = source
-        self.premises = {}
+        self.premises: Dict[PremiseFactory()] = {}
 
-    def insert_premise(name, premise):
+    def insert_premise(self, name: str, premise_factory: PremiseFactory):
         """
-        Receives an String and an DataPremise instance.
+        Receives the name of premise and an DataPremise class.
         Must insert the DataPremise instance on the premises dictionary using name as key.
         Must overwrite the DataPremise.name for the name passed on parameter.
         Returns None
         """
-        pass
+        premise = premise_factory(node=self, name=name)
+        self.premises.update({name: premise})
 
-    def remove_premise(premise):
+    def remove_premise(self, premise):
         """
         Receives an String or an DataPremise instance.
         Must remove the DataPremise instance on the premises dictionary.
