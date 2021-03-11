4175cd6027ba4711df0909ba38661e040b9a0a4e
Jubel <jubelcassioverridasilva@gmail.com>
2021-03-11 12:00:05 -0300

    test: :white_check_mark: adding tests to DataPremises

---
+++ test/test_data_premise.py
@@ -0,0 +1,39 @@
+import pytest
+
+from pipeline_penguin.data_premise import DataPremise
+from pipeline_penguin.data_premise_sql import DataPremiseSQL
+
+
+@pytest.fixture()
+def data_premise_arguments():
+    yield {
+        "name": "name_test",
+        "type": "type_test",
+        "column": "column_test"
+    }
+
+
+@pytest.fixture()
+def data_premise_sql_arguments():
+    yield {
+        "name": "name_test",
+        "type": "type_test",
+        "column": "column_test",
+        "query": "SELECT * FROM `table`"
+    }
+
+
+class TestDataPremise:
+    def test_instance_type(self, data_premise_arguments):
+        data_premise = DataPremise(**data_premise_arguments)
+        assert isinstance(data_premise, DataPremise)
+
+
+class TestDataPremiseSQL:
+    def test_instance_type(self, data_premise_sql_arguments):
+        data_premise_sql = DataPremiseSQL(**data_premise_sql_arguments)
+        assert isinstance(data_premise_sql, DataPremise)
+
+    def test_instance_superclass_type(self, data_premise_sql_arguments):
+        data_premise_sql = DataPremiseSQL(**data_premise_sql_arguments)
+        assert isinstance(data_premise_sql, DataPremiseSQL)
