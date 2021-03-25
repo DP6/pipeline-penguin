3f837020ec6f941ea278bdc45bad3deda2183d28
Jubel <jubelcassioverridasilva@gmail.com>
2021-03-25 12:27:38 -0300

    test: Adding tests for abstract connectors
    Adding tests for connector and connector_sql modules

---
+++ test/test_connector_sql.py
@@ -0,0 +1,15 @@
+from pipeline_penguin.connector import Connector
+from pipeline_penguin.connector_sql import ConnectorSQL
+
+
+class TestConnectorSQL:
+
+    def test_instance_class_type(self):
+        assert isinstance(ConnectorSQL(), ConnectorSQL)
+
+    def test_instance_superclass_type(self):
+        assert isinstance(ConnectorSQL(), Connector)
+
+    def test_connector_type(self):
+        conn = ConnectorSQL()
+        assert conn.type == "SQL"
