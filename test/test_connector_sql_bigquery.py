200768985421980acd2d3c1739315ca1d7d32f4f
Jubel <jubelcassioverridasilva@gmail.com>
2021-03-25 13:46:54 -0300

    test: Added more tests for connector_sql_bigquery
    Adding more tests for the 'run' method of the BigQuery Connector and refactoring the test fixtures.

--- test/test_connector_sql_bigquery.py
+++ test/test_connector_sql_bigquery.py
@@ -10,53 +10,80 @@ import pytest
 
 
 @pytest.fixture
+def mock_from_service_account_file(monkeypatch):
+    def mock_function(credentials_path):
+        return
+
+    monkeypatch.setattr(Credentials, "from_service_account_file", mock_function)
+
+
+@pytest.fixture
+def mock_pandas_read_gbq(monkeypatch):
+    def mock_function(query, credentials, max_results):
+        return pd.DataFrame([i for i in range(max_results)])
+
+    monkeypatch.setattr(pd, "read_gbq", mock_function)
+
+
+@pytest.fixture
 def mock_isfile(monkeypatch):
-    def mock_result(path):
+    def mock_function(path):
         return path == "true_file.json"
 
-    monkeypatch.setattr(path, "isfile", mock_result)
+    monkeypatch.setattr(path, "isfile", mock_function)
 
 
 class TestConnectorSQLBigQuery:
     def test_instance_class_type(self, mock_isfile):
         assert isinstance(
-            ConnectorSQLBigQuery(credentials_path="true_file.json", max_rows=50),
+            ConnectorSQLBigQuery(credentials_path="true_file.json"),
             ConnectorSQLBigQuery,
         )
 
     def test_instance_superclass_type(self, mock_isfile):
         assert isinstance(
-            ConnectorSQLBigQuery(credentials_path="true_file.json", max_rows=50),
+            ConnectorSQLBigQuery(credentials_path="true_file.json"),
             ConnectorSQL,
         )
 
     def test_connector_type(self, mock_isfile):
-        conn = ConnectorSQLBigQuery(credentials_path="true_file.json", max_rows=50)
+        conn = ConnectorSQLBigQuery(credentials_path="true_file.json")
         assert conn.type == "SQL"
 
     def test_connector_source(self, mock_isfile):
-        conn = ConnectorSQLBigQuery(credentials_path="true_file.json", max_rows=50)
+        conn = ConnectorSQLBigQuery(credentials_path="true_file.json")
         assert conn.source == "BigQuery"
 
     def test_invalid_filename(self, monkeypatch, mock_isfile):
         with pytest.raises(FileNotFoundError):
-            conn = ConnectorSQLBigQuery(credentials_path="fake_file.json", max_rows=50)
+            conn = ConnectorSQLBigQuery(credentials_path="fake_file.json")
 
-    def test_connection_run_returns_dataframe(self, monkeypatch, mock_isfile):
-        def mock_from_service_account_file(credentials_path):
-            return
 
-        def mock_pandas_read_gbq(query, credentials, max_results):
-            return pd.DataFrame([i for i in range(50)])
-
-        monkeypatch.setattr(
-            Credentials, "from_service_account_file", mock_from_service_account_file
-        )
-        monkeypatch.setattr(pd, "read_gbq", mock_pandas_read_gbq)
-
-        conn = ConnectorSQLBigQuery(credentials_path="true_file.json", max_rows=50)
-
-        result = conn.run("SELECT * FROM `project.dataset.table`", 50)
-
+class TestConnectorSQLBigQueryRunExecution:
+    def test_returns_dataframe(
+        self, mock_isfile, mock_from_service_account_file, mock_pandas_read_gbq
+    ):
+        conn = ConnectorSQLBigQuery(credentials_path="true_file.json")
+        result = conn.run("SELECT * FROM `project.dataset.table`")
         assert isinstance(result, pd.DataFrame)
+
+    def test_default_dataframe_size(
+        self, mock_isfile, mock_from_service_account_file, mock_pandas_read_gbq
+    ):
+        conn = ConnectorSQLBigQuery(credentials_path="true_file.json")
+        result = conn.run("SELECT * FROM `project.dataset.table`")
+        assert result.size == 1000
+
+    def test_override_default_dataframe_size(
+        self, mock_isfile, mock_from_service_account_file, mock_pandas_read_gbq
+    ):
+        conn = ConnectorSQLBigQuery(credentials_path="true_file.json", max_rows=50)
+        result = conn.run("SELECT * FROM `project.dataset.table`")
+        assert result.size == 50
+
+    def test_override_dataframe_size(
+        self, mock_isfile, mock_from_service_account_file, mock_pandas_read_gbq
+    ):
+        conn = ConnectorSQLBigQuery(credentials_path="true_file.json")
+        result = conn.run("SELECT * FROM `project.dataset.table`", max_rows=50)
         assert result.size == 50
