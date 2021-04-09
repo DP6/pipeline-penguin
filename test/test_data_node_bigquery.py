9854efa108b9994646e1c53ca3dbea228a55170a
Jubel <jubelcassioverridasilva@gmail.com>
2021-04-09 13:48:10 -0300

    feat: :art: DataPremise validation with PremiseTypes.
    Refactoring the way PremiseTypes and NodeTypes were implemented. Adding validation on the datanodes for inserting new data_premisses.

--- test/test_data_node_bigquery.py
+++ test/test_data_node_bigquery.py
@@ -1,6 +1,6 @@
-from pipeline_penguin import NodeType
-from pipeline_penguin.core.data_node import DataNode
+from pipeline_penguin.core.data_node import DataNode, NodeType
 from pipeline_penguin.data_node import DataNodeBigQuery
+from pipeline_penguin.data_premise.sql.check_null import DataPremiseSQLCheckNull
 
 
 class TestDataNodeBigQuery:
@@ -17,4 +17,24 @@ class TestDataNodeBigQuery:
         )
 
     def test_node_type_reference(self):
-        assert DataNodeBigQuery == NodeType.BIG_QUERY
+        data_node = DataNodeBigQuery(
+            "name_test",
+            "project_test",
+            "dataset_test",
+            "table_test",
+            "account_test",
+        )
+        assert data_node.source == NodeType.BIG_QUERY
+
+    def test_insert_premise(self):
+        data_node = DataNodeBigQuery(
+            "name_test",
+            "project_test",
+            "dataset_test",
+            "table_test",
+            "account_test",
+        )
+
+        data_node.insert_premise(
+            "check_nulls", DataPremiseSQLCheckNull, column="test_column"
+        )
