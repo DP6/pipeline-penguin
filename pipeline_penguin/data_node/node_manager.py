2d0a11483df5966d4f4ae98271061302602105b5
Jubel <jubelcassioverridasilva@gmail.com>
2021-04-09 13:48:10 -0300

    feat: :art: DataPremise validation with PremiseTypes.
    Refactoring the way PremiseTypes and NodeTypes were implemented. Adding validation on the datanodes for inserting new data_premisses.

--- pipeline_penguin/data_node/node_manager.py
+++ pipeline_penguin/data_node/node_manager.py
@@ -1,3 +1,5 @@
+"""This module provides the NodeManager constructor."""
+
 import copy
 import inspect
 from typing import Type, Optional, Union, Any
@@ -10,44 +12,49 @@ from pipeline_penguin.exceptions import (
 
 
 class NodeManager:
+    """Singleton interface for managing all DataNodes."""
+
     _instance = None
 
     def __new__(cls):
-        """
-        NodeManager use Singleton Design Pattern
-        """
+        """Magic method for maintaining the singleton pattern."""
         if not cls._instance:
             cls._instance = super(NodeManager, cls).__new__(cls)
         return cls._instance
 
     def __init__(self):
-        """
-        Constructor for the NodeManager.
-        Attributes are all privates.
-        """
+        """Initialize the manager."""
         self.__nodes = {}
 
     @staticmethod
-    def _is_data_node_class(node_type: Any) -> bool:
+    def _is_data_node_class(node_factory: Any) -> bool:
+        """Return whether the given class is a DataNode subclass or not.
+
+        Args:
+            node_factory: Constructor for a DataNode object
+        """
         return (
-            node_type != DataNode
-            and inspect.isclass(node_type)
-            and issubclass(node_type, DataNode)
+            node_factory != DataNode
+            and inspect.isclass(node_factory)
+            and issubclass(node_factory, DataNode)
         )
 
-    @staticmethod
-    def _is_data_node_instance(node_type) -> bool:
-        return isinstance(node_type, DataNode)
+    def create_node(
+        self, name: str, node_factory: Type[DataNode], *args, **kwargs
+    ) -> DataNode:
+        """Instantiate a DataNode with the proveided arguments.
 
-    def create_node(self, name: str, node_type: Type[DataNode], args: dict) -> DataNode:
-        """
-        Initiate a DataNode with the inputted data.
-        Returns a DataNode.
+        Args:
+            name: Name of the DataNode
+            node_factory: Constructor for a DataNode object
+            *args, **kwargs: Arguments used on the DataNode's initialization.
+        Raises:
+            WrongTypeReference: If the provided constructor is not a subclass of DataNode
+            NodeManagerMissingCorrectArgs: When the node was not able to be initialized with the provided arguments
         """
         try:
-            if self._is_data_node_class(node_type):
-                args.update({"name": name})
-                node = node_type(**args)
+            if self._is_data_node_class(node_factory):
+                node = node_factory(name, *args, **kwargs)
             else:
                 raise WrongTypeReference("DataNode should be of type NodeType")
         except TypeError as e:
@@ -57,38 +64,33 @@ class NodeManager:
         return node
 
     def get_node(self, name: str) -> Optional[Type[DataNode]]:
-        """
-        Get a DataNode that already exists.
-        Returns a DataNode.
+        """Retrieve a previously created DataNode by its name.
+
+        Args:
+            name: name of the data node to retrieve
         """
         return self.__nodes.get(name)
 
     def list_nodes(self) -> list[Type[DataNode]]:
-        """
-        Prints every DataNode name on the nodes dictionary.
-        Returns list of DataNode.
-        """
+        """List all existing data nodes."""
         for key in self.__nodes.keys():
             print(key)
 
         return list(self.__nodes.keys())
 
     def remove_node(self, name: str) -> None:
-        """
-        Remove a DataNode from the nodes dictionary, then turn it into None.
-        Returns None.
-        """
-
+        """Remove a previously added DataNode."""
         if name in self.__nodes:
             del self.__nodes[name]
 
     def copy_node(self, node: Union[str, DataNode], name: str) -> Optional[DataNode]:
-        """
-        Deep copies a DataNode with a new name.
-        Returns a DataNode, with the name was not found return None.
-        """
+        """Create a copy of the given DataNode with a new name.
 
-        if self._is_data_node_instance(node):
+        Args:
+            node: Primary DataNode
+            name: Name of the DataNode copy
+        """
+        if isinstance(node, DataNode):
             copied_node = copy.deepcopy(node)
         elif isinstance(node, str):
             copied_node = copy.deepcopy(self.__nodes.get(node))
