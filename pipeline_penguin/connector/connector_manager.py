6078b6d703ff36b263f2e2989df685a695ef1108
Bruno Munhoz <bruno.munhoz@dp6.com.br>
2021-04-16 16:27:03 -0300

    chore: Refatoring ConnectorManager

--- pipeline_penguin/connector/connector_manager.py
+++ pipeline_penguin/connector/connector_manager.py
@@ -2,8 +2,10 @@
 import inspect
 from typing import Type, Any
 from pipeline_penguin.core.connector import Connector
-from pipeline_penguin.exceptions import WrongTypeReference, ConnectorManagerMissingCorrectArgs
-
+from pipeline_penguin.exceptions import (
+    WrongTypeReference,
+    ConnectorManagerMissingCorrectArgs,
+)
 
 
 class ConnectorManager:
@@ -27,7 +29,7 @@ class ConnectorManager:
     @staticmethod
     def _is_connector_subclass(connector_type: Any) -> bool:
         """
-        Verify with class passed is child of Connector class
+        Verify if class is a subclass of Connector class
         """
         return (
             connector_type != Connector
@@ -35,27 +37,22 @@ class ConnectorManager:
             and issubclass(connector_type, Connector)
         )
 
-    def _get_dict_key(self, **kwargs):
+    def _get_dict_key(self, connector):
         try:
-            if 'connector' in kwargs and self._is_connector_subclass(kwargs['connector'].__class__):
-                connector = kwargs['connector']
+            if self._is_connector_subclass(
+                connector.__class__
+            ) or self._is_connector_subclass(connector):
                 return connector.type + connector.source
-            elif 'connector_type' in kwargs and self._is_connector_subclass(kwargs['connector_type']):
-                connector_type = kwargs['connector_type']
-                return connector_type.type + connector_type.source
-            elif 'type' in kwargs and isinstance(kwargs['type'], str) and 'source' in kwargs and isinstance(kwargs['source'], str):
-                return kwargs['type'] + kwargs['source']
-            else:
-                raise ConnectorManagerMissingCorrectArgs("Invalid arguments")
         except TypeError as e:
             raise ConnectorManagerMissingCorrectArgs(str(e))
 
     def define_default(self, connector: Connector) -> None:
-        """
-        """
+        """"""
         try:
             if self._is_connector_subclass(connector.__class__):
-                self.__default_connectors.update({self._get_dict_key(connector = connector): connector})
+                self.__default_connectors.update(
+                    {self._get_dict_key(connector=connector): connector}
+                )
             else:
                 raise WrongTypeReference("Connector should be of type Connector")
         except TypeError as e:
@@ -63,16 +60,16 @@ class ConnectorManager:
 
         return None
 
-    def get_default(self, **kwargs):
-        """
-        """
-        return self.__default_connectors.get(self._get_dict_key(**kwargs))
+    def get_default(self, connector: Connector):
+        """"""
+        return self.__default_connectors.get(self._get_dict_key(connector))
 
-    def remove_default(self, **kwargs):
-        """
-        """
-        dict_key = self._get_dict_key(**kwargs)
+    def remove_default(self, connector: Connector):
+        """"""
+        dict_key = self._get_dict_key(connector)
         if dict_key in self.__default_connectors:
             removed_connector = self.__default_connectors[dict_key]
             del self.__default_connectors[dict_key]
             return removed_connector
+
+        return None
