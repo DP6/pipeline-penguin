9f73793518cbdaa360461992393eb0a7395a51be
Bruno Munhoz <bruno.munhoz@dp6.com.br>
2021-04-23 14:33:37 -0300

    fix: Fixing docstring position

--- pipeline_penguin/core/data_premise/sql.py
+++ pipeline_penguin/core/data_premise/sql.py
@@ -3,16 +3,16 @@ from . import DataPremise, PremiseType
 
 
 class DataPremiseSQL(DataPremise):
+    """Constructor for the DataPremiseSQL.
+
+    Args:
+        name: name for the data premise
+        column: column to be read by the premise
+        query: SQL query to be executed for premise validation
+    Attributes:
+        type: type indicator of the premise. It is always "SQL"
+    """
+
     def __init__(self, name, column, query):
-        """Constructor for the DataPremiseSQL.
-
-        Args:
-            name: name for the data premise
-            column: column to be read by the premise
-            query: SQL query to be executed for premise validation
-        Attributes:
-            type: type indicator of the premise. It is always "SQL"
-        """
-
         super().__init__(name, PremiseType.SQL, column)
         self.query = query
