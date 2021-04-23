fae7180999ba5223b24aeaa3102b3ac1a29ca9cf
Jubel <jubelcassioverridasilva@gmail.com>
2021-04-23 16:49:29 -0300

    test: :white_check_mark: Tests for premise validation
    Updating tests for premise validation methods.

--- test/test_data_node_bigquery.py
+++ test/test_data_node_bigquery.py
@@ -3,12 +3,41 @@ from pipeline_penguin.data_node import DataNodeBigQuery
 from pipeline_penguin.data_premise.sql.check_null import DataPremiseSQLCheckNull
 from pipeline_penguin.core.data_premise.data_premise import DataPremise
 import pytest
+from os import path
 from pipeline_penguin.exceptions import WrongTypeReference
+from pipeline_penguin.connector.sql.bigquery import ConnectorSQLBigQuery
+from pipeline_penguin.connector.connector_manager import ConnectorManager
+import unittest.mock
 
 
-class MockPremise(DataPremise):
-    def __init__(self, name, data_node, column):
-        super().__init__(name, "MOCK", column)
+@pytest.fixture()
+def _mock_premise():
+    def mock_premise():
+        class MockPremise(DataPremise):
+            type = "MOCK"
+
+            def __init__(self, name, data_node, column):
+                super().__init__(name, data_node)
+
+        return MockPremise
+
+    yield mock_premise
+
+
+@pytest.fixture
+def _mock_isfile(monkeypatch):
+    def mock_function(path):
+        return path == "true_file.json"
+
+    monkeypatch.setattr(path, "isfile", mock_function)
+
+
+@pytest.fixture
+def _mock_connector_manager(monkeypatch):
+    def mock_function(self, premise_type, source):
+        return "ConnectorSQLBigQuery"
+
+    monkeypatch.setattr(ConnectorManager, "get_default", mock_function)
 
 
 class TestDataNodeBigQuery:
@@ -47,7 +76,7 @@ class TestDataNodeBigQuery:
             "check_nulls", DataPremiseSQLCheckNull, column="test_column"
         )
 
-    def test_raises_error_when_inserting_unsupported_premise(self):
+    def test_raises_error_when_inserting_unsupported_premise(self, _mock_premise):
         data_node = DataNodeBigQuery(
             "name_test",
             "project_test",
@@ -57,4 +86,41 @@ class TestDataNodeBigQuery:
         )
 
         with pytest.raises(WrongTypeReference):
-            data_node.insert_premise("check_nulls", MockPremise, column="test_column")
+            data_node.insert_premise(
+                "check_nulls", _mock_premise(), column="test_column"
+            )
+
+    def test_get_internal_connector(self, _mock_isfile):
+        data_node = DataNodeBigQuery(
+            "name_test",
+            "project_test",
+            "dataset_test",
+            "table_test",
+            "account_test",
+        )
+
+        mock_connector = ConnectorSQLBigQuery("true_file.json")
+
+        data_node.connectors["SQLBigQuery"] = mock_connector
+        assert data_node.get_connector("SQL") == mock_connector
+
+    def test_get_connector_from_manager(self, _mock_isfile, _mock_connector_manager):
+        data_node = DataNodeBigQuery(
+            "name_test",
+            "project_test",
+            "dataset_test",
+            "table_test",
+            "account_test",
+        )
+        assert data_node.get_connector("SQL") == "ConnectorSQLBigQuery"
+
+    def test_raises_error_when_connector_is_not_found(self):
+        data_node = DataNodeBigQuery(
+            "name_test",
+            "project_test",
+            "dataset_test",
+            "table_test",
+            "account_test",
+        )
+        with pytest.raises(WrongTypeReference):
+            data_node.get_connector("SQL")
---
+++ test/test_data_premise_check_null.py
@@ -0,0 +1,65 @@
+import pytest
+
+from unittest.mock import MagicMock
+import pandas as pd
+from pipeline_penguin.data_premise.sql import DataPremiseSQLCheckNull
+from pipeline_penguin.core.data_node.data_node import DataNode
+from pipeline_penguin.core.connector.connector import Connector
+from pipeline_penguin.data_node.sql.bigquery import DataNodeBigQuery
+
+
+@pytest.fixture
+def _mock_data_node_with_passed_validation(monkeypatch):
+    def mock_function(self, type):
+        connector_mock = Connector("SQL")
+        connector_mock.run = MagicMock(return_value=pd.DataFrame())
+        return connector_mock
+
+    monkeypatch.setattr(DataNodeBigQuery, "get_connector", mock_function)
+
+
+@pytest.fixture
+def _mock_data_node_with_failed_validation(monkeypatch):
+    def mock_function(self, type):
+        connector_mock = Connector("SQL")
+        connector_mock.run = MagicMock(return_value=pd.DataFrame([1, 2, 3]))
+        return connector_mock
+
+    monkeypatch.setattr(DataNodeBigQuery, "get_connector", mock_function)
+
+
+class TestDataPremiseSQLCheckNull:
+    def test_instance_type(self, _mock_data_node_with_passed_validation):
+        data_node = DataNodeBigQuery(
+            "mock_node",
+            "project_test",
+            "dataset_test",
+            "table_test",
+            "service_account.json",
+        )
+        data_premise = DataPremiseSQLCheckNull("test_name", data_node, "test_column")
+        assert isinstance(data_premise, DataPremiseSQLCheckNull)
+
+    def test_passing_validate(self, _mock_data_node_with_passed_validation):
+        data_node = DataNodeBigQuery(
+            "mock_node",
+            "project_test",
+            "dataset_test",
+            "table_test",
+            "service_account.json",
+        )
+        data_premise = DataPremiseSQLCheckNull("test_name", data_node, "test_column")
+        result = data_premise.validate()
+        assert result is True
+
+    def test_failing_validate(self, _mock_data_node_with_failed_validation):
+        data_node = DataNodeBigQuery(
+            "mock_node",
+            "project_test",
+            "dataset_test",
+            "table_test",
+            "service_account.json",
+        )
+        data_premise = DataPremiseSQLCheckNull("test_name", data_node, "test_column")
+        result = data_premise.validate()
+        assert result is False
