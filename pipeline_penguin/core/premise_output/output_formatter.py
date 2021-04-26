d6e28a6aacf8cef33d00d1e1e74644b974256678
Bruno Munhoz <bruno.munhoz@dp6.com.br>
2021-04-26 15:53:19 -0300

    feat: Creating Basic Output Classes

---
+++ pipeline_penguin/core/premise_output/output_formatter.py
@@ -0,0 +1,9 @@
+"""This module provides the abstract OutputFormatter constructor."""
+
+
+class OutputFormatter:
+    """A OutputFormatter represents a way to format the PremiseOutput class"""
+
+    def export_output(self, premise_output: "PremiseOutput"):
+        """Generate an formated output from a PremiseOutput class"""
+        pass
