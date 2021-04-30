a8812b903a1db9a692a146147c00e9cfd3f1877a
Jubel <jubelcassioverridasilva@gmail.com>
2021-04-30 10:40:34 -0300

    feat: :sparkles: Finishing up log formatter
    Modifying OutputFormatterLog to return a readable message.

--- pipeline_penguin/premise_output/output_formatter_log.py
+++ pipeline_penguin/premise_output/output_formatter_log.py
@@ -4,11 +4,20 @@ import json
 
 
 class OutputFormatterLog(OutputFormatter):
-    """A OutputFormatterLog represents a way to format the PremiseOutput class as an log"""
+    """A OutputFormatterLog represents a way to format the PremiseOutput class as readable log"""
 
-    def export_output(self, premise_output: "PremiseOutput"):
-        """Generate an formated output from a PremiseOutput class"""
+    def export_output(self, premise_output: "PremiseOutput") -> str:
+        """Construct a human-readable message containing the results of a premise validation.
+
+        Args:
+            premise_output: PremiseOutput object to be formatted
+        Returns:
+            str: Human-readable message with PremiseOutput's data
+        """
         data_premise = premise_output.data_premise
-        output_data = data_premise.to_serializeble_dict()
+        output_data = premise_output.to_serializeble_dict()
 
-        return json.dumps(output_data)
+        json_data = json.dumps(output_data, indent=4, sort_keys=True)
+
+        msg = f"""Results of {data_premise.name} validation:\n{json_data}"""
+        return msg
