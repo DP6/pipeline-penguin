91585b1c7f2b450e327b14156e4c47358de32b53
JubelCassio <jubelcassioverridasilva@gmail.com>
2021-08-16 09:38:34 -0300

    feat: :sparkles: Introducing Raft Suite Hub (RSH) Formatter

--- pipeline_penguin/premise_output/output_formatter_rsh.py
+++ pipeline_penguin/premise_output/output_formatter_rsh.py
@@ -1,4 +1,4 @@
-r"""Contains the `OutputFormatterRHS` constructor, used to sent data_premises results to an RHS cloud function.
+r"""Contains the `OutputFormatterRSH` constructor, used to sent data_premises results to an RSH cloud function.
 
 It sends a request with parameters:
 
@@ -22,27 +22,80 @@ Location: pipeline_penguin/core/premise_output/
 Example usage:
 
 ```python
-formatter = OutputFormatterRHS()
+formatter = OutputFormatterRSH()
 formatter.export_output(premise_output)
 ```
 """
 
+
 from pipeline_penguin.core.premise_output.output_formatter import OutputFormatter
+from google.oauth2.service_account import Credentials
+
+import google.auth
+from os import path
 
 
-class OutputFormatterRHS(OutputFormatter):
-    """Contains the `OutputFormatterRHS` constructor, used to sent data_premises results to an RHS cloud function."""
+def make_authorized_post_request(service_url, body, credentials):
+    """
+    make_authorized_get_request makes a GET request to the specified HTTP endpoint
+    in service_url (must be a complete URL) by authenticating with the
+    ID token obtained from the google-auth client library.
+    """
+    auth_sess = google.auth.transport.requests.AuthorizedSession(credentials)
+    resp = auth_sess.post(service_url, json=body)
+    return resp
+
+
+class OutputFormatterRSH(OutputFormatter):
+    """Contains the `OutputFormatterRSH` constructor, used to sent data_premises results to an RSH cloud function."""
 
     def export_output(
-        self, premise_output: "PremiseOutput", url: str, project: str
+        self,
+        premise_output: "PremiseOutput",
+        url: str,
+        project: str,
+        spec: str,
+        deploy: str,
+        code: str,
+        description: str,
+        credentials_path: str = "default",
     ) -> str:
-        """Send a request to structure defined by Raft Suit Hub.
+        """Send a request to structure defined by Raft Suite Hub.
 
         Args:
             premise_output: PremiseOutput object to be formatted
-            url: URL for the RHS cloud function
+            url: URL for the RSH cloud function
             project: Project name running the validations
+            spec:
+            deploy:
+            code:
+            description:
+            credentials_path: Path to service_account for authentication
         Returns:
             None
         """
-        pass
+
+        if credentials_path == "default":
+            print("Using google.auth.default credentials")
+            credentials, project_id = google.auth.default()
+        elif path.isfile(credentials_path):
+            credentials = Credentials.from_service_account_file(credentials_path)
+
+        body = {
+            "project": project,
+            "module": "pipeline-penguin",
+            "spec": spec,
+            "deploy": deploy,
+            "code": code,
+            "description": description,
+            "payload": {
+                "data_premise": premise_output.data_premise,
+                "data_node": premise_output.data_node,
+                "column": premise_output.column,
+                "pass_validation": premise_output.pass_validation,
+                "failed_count": premise_output.failed_count,
+                "failed_values": premise_output.failed_values.to_json(),
+            },
+        }
+
+        return make_authorized_post_request(url, body, credentials)
