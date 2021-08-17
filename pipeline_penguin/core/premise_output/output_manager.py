d2a17679beb2d4151a6a40af422b33962cee9b76
Bruno Munhoz <bruno.munhoz@dp6.com.br>
2021-08-17 14:46:55 -0300

    docs: documenting the new output structure

--- pipeline_penguin/core/premise_output/output_manager.py
+++ pipeline_penguin/core/premise_output/output_manager.py
@@ -1,7 +1,7 @@
 r"""Core premise_output module, contains the `OutputManager` class.
 
 OutputManager is a Singleton which stores every `PremiseOutput` returned by the validations. It can
-be used to mass format all PremiseOutputs on the same OutputFormatter type.
+be used to mass format and export PremiseOutputs.
 
 Location: pipeline_penguin/core/premise_output/
 
@@ -24,20 +24,29 @@ class OutputManager:
     """An OutputManager stores "PremiseOutputs" returned by DataPremises validations.
 
     Attributes:
-        outputs: Dictionary structure containing the PremiseOutputs
+        outputs: Dictionary structure containing data for every PremiseOutput and its formatation and exportation. The structure is the following:
+
+        | Key                                             | Value Type    | Description                                                                      |
+        |-------------------------------------------------|---------------|----------------------------------------------------------------------------------|
+        | [{node_name}]                                   | Dictionary    | Dictionary for every premise executed on this DataNode                           |
+        | [{node_name}][{premise_name}]                   | Dictionary    | Dictionary containing details of a DataPremise on a DataNode                     |
+        | [{node_name}][{premise_name}][output]           | PremiseOutput | An instance of a PremiseOutput detailing the results of a DataPremise validation |
+        | [{node_name}][{premise_name}][export_status]    | Bool          | If the export was successful or not                                              |
+        | [{node_name}][{premise_name}][formatted_output] | *             | The formatted output used in the exporter                                        |
     """
 
     def __init__(self):
         """Initialization of the DataPremise."""
         self.outputs = {}
 
-    def format_outputs(self, formatter: "OutputFormatter"):
-        """Method for applyting an OutputFormatter over every PremiseOutput.
+    def export_outputs(self, formatter: "OutputFormatter"):
+        """Method for applying an OutputFormatter over every PremiseOutput. Then exporting it using an OutputExporter
 
         Args:
             formatter: The OutputFormatter instance to be applied on every PremiseOutput.
+            exporter: The OutputExporter instance to export every PremiseOutput after applying an formatation.
         Returns:
-            A `dictionary` containing the formatted results.
+            The outputs attribute. A `dictionary` containing data for every PremiseOutput and its formatation and exportation.
         """
         results = {}
 
